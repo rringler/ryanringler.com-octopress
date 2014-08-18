0af0f43fa2edac3e735795869f7ef61111c7552a
Ryan Ringler <rringler@gmail.com>
2014-08-17 17:34:09 -0700

    Adds Screenshots to the Projects section

---
+++ public/javascripts/custom.js
@@ -0,0 +1,35 @@
+function carouselNormalization() {
+  var items   = $('#carousel-github-projects .item'), // grab all slides
+      heights = [],                                   // array to store heights
+      tallest;                                        // tallest slide
+
+  if (items.length) {
+    function normalizeHeights() {
+      items.each(function() {
+        heights.push($(this).height());               // add heights to array
+      });
+
+      tallest = Math.max.apply(null, heights);        // find the largest height
+
+      items.each(function() {
+        $(this).css('min-height', tallest + 'px');
+      });
+    };
+
+    normalizeHeights();
+
+    $(window).on('resize orientationchange', function() {
+      tallest = 0, heights.length = 0;               // reset the variables
+
+      items.each(function() {
+        $(this).css('min-height', '0');              // reset each item's height
+      });
+
+      normalizeHeights();                            // run it again
+    });
+  }
+}
+
+$('document').ready(function() {
+  carouselNormalization();
+});
